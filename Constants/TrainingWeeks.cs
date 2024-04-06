b54b46d11983def9b781c9b67be1925badb07e54
Michael Buchoff <mbuchoff@gmail.com>
2024-04-06 09:34:36 -0400

    Added 3s blueberries to morning oatmeal

--- Constants/TrainingWeeks.cs
+++ Constants/TrainingWeeks.cs
@@ -73,7 +73,8 @@ internal static class TrainingWeeks
     public static TrainingWeek MuscleGain3TrainingWeek { get; } = new(
         nonworkoutMeals:
         [
-            new("Waking", new Macros(P: PROTEIN_PER_MEAL_ON_NONWORKOUT_DAY, F: 10, C: 60), FoodGroupings.OatmealAndEdamame),
+            new("Waking", new Macros(P: PROTEIN_PER_MEAL_ON_NONWORKOUT_DAY, F: 10, C: 60) -
+                (Foods.Blueberries_1_Scoop.NutritionalInformation.Macros * 3), FoodGroupings.OatmealAndEdamame),
             new("3-5 hours after last meal",
                 new Macros(P: PROTEIN_PER_MEAL_ON_NONWORKOUT_DAY, F: 20, C: 60),
                 FoodGroupings.Seitan),
@@ -84,7 +85,8 @@ internal static class TrainingWeeks
                 new Macros(P: PROTEIN_PER_MEAL_ON_NONWORKOUT_DAY, F: 20, C: 60),
                 FoodGroupings.FarroAndEdamame),
             new("Bedtime",
-                new Macros(P: PROTEIN_PER_MEAL_ON_NONWORKOUT_DAY, F: 25, C: 0) - Foods.AlmondMilk_2_Cup.NutritionalInformation.Macros,
+                new Macros(P: PROTEIN_PER_MEAL_ON_NONWORKOUT_DAY, F: 25, C: 0) -
+                Foods.AlmondMilk_2_Cup.NutritionalInformation.Macros,
                 new(Foods.PeaProtein_1_Scoop,
                     Foods.AlmondButter_1_Tbsp,
                     Foods.Oatmeal_Walmart_1_Scoop)),  // Will come up slightly negative, subtract from first meal
@@ -92,7 +94,8 @@ internal static class TrainingWeeks
         runningMeals:
         [
             new("1-3 hours before workout",
-                new Macros(P: PROTEIN_PER_MEAL_ON_WORKOUT_DAY, F: 20, C: 80), FoodGroupings.OatmealWithProteinPowder),
+                new Macros(P: PROTEIN_PER_MEAL_ON_WORKOUT_DAY, F: 20, C: 80) -
+                (Foods.Blueberries_1_Scoop.NutritionalInformation.Macros * 3), FoodGroupings.OatmealWithProteinPowder),
             new("40 minutes after workout",
                 new(P: PROTEIN_PER_MEAL_ON_WORKOUT_DAY, F: 10, C: 120),
                 FoodGroupings.Seitan),
@@ -107,7 +110,7 @@ internal static class TrainingWeeks
         xfitMeals:
         [
             new("1-3 hours before workout",
-                new Macros(P: PROTEIN_PER_MEAL_ON_WORKOUT_DAY, F: 20, C: 80), FoodGroupings.OatmealWithProteinPowder),
+                new Macros(P: PROTEIN_PER_MEAL_ON_WORKOUT_DAY, F: 20, C: 80) - (Foods.Blueberries_1_Scoop.NutritionalInformation.Macros * 3), FoodGroupings.OatmealWithProteinPowder),
             new("40 minutes after workout",
                 new(P: PROTEIN_PER_MEAL_ON_WORKOUT_DAY, F: 10, C: 120), FoodGroupings.Seitan),
             new("2-4 hours after last meal",
