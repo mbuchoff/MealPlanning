e337693e728a9e410b3653d267bfff89e93789a4
Michael Buchoff <mbuchoff@gmail.com>
2024-04-28 07:19:21 -0400

    Added total macros per week

--- Constants/FoodGroupings.cs
+++ Constants/FoodGroupings.cs
@@ -19,15 +19,18 @@ internal static class FoodGroupings
     public static FoodGrouping ProteinShake { get; } =
         new("protein shake", [new(Foods.AlmondMilk_2_Cup, Servings: 1)],
         Foods.PeaProtein_1_Scoop, Foods.ChiaSeeds_2_5_Tbsp, Foods.Blueberries_1_Scoop);
+
     public static FoodGrouping Seitan { get; } =
         new("seitan", Foods.Seitan_Walmart_Yeast_1_Gram_Gluten_4x, Foods.OliveOil_1_Tbsp, Foods.WheatBerries_45_Grams);
+
     public static FoodGrouping Tofu { get; } =
         new("tofu", Foods.Tofu_1_5_Block, Foods.PumpkinSeeds_30_Grams, Foods.Farro_52_Gram);
+
     public static FoodGrouping RiceAndEdamame { get; } =
         new("rice and edamame", Foods.Edamame_35_Grams, Foods.PumpkinSeeds_30_Grams, Foods.BrownRice_45_Grams);
-    public static FoodGrouping WheatBerriesRice { get; } =
-    new("wheat berries and rice",
+
+    public static FoodGrouping WheatBerriesRice { get; } = new(
+        "wheat berries and rice",
         [new(Foods.Farro_52_Gram, Servings: 2)],
         Foods.WheatBerries_45_Grams, Foods.OliveOil_1_Tbsp, Foods.BrownRice_45_Grams);
-
 }
--- Macros.cs
+++ Macros.cs
@@ -6,9 +6,39 @@ public record Macros(double P, double F, double C)
     public static Macros operator -(Macros m1, Macros m2) => m1 + (-m2);
     public static Macros operator +(Macros m1, Macros m2) => new(m1.P + m2.P, m1.F + m2.F, m1.C + m2.C);
     public static Macros operator *(Macros m, double d) => new(m.P * d, m.F * d, m.C * d);
+    public static Macros operator *(double d, Macros m) => (m * d);
+    public static Macros operator /(Macros m, double d) => (m * (1/d));
 
     public Macros CloneWithTweakedMacros(double pMultiplier, double fMultiplier, double cMultiplier) =>
         new(P * pMultiplier, F * fMultiplier, C * cMultiplier);
+
+    public override string ToString()
+    {
+        var macroDetails = new[]
+        {
+            new
+            {
+                Grams = P,
+                Name = "P",
+                CalsPerGram = 4,
+            },
+            new
+            {
+                Grams = F,
+                Name = "F",
+                CalsPerGram = 9,
+            },
+            new
+            {
+                Grams = C,
+                Name = "C",
+                CalsPerGram = 4,
+            },
+        };
+        var totalCals = macroDetails.Sum(m => m.CalsPerGram * m.Grams);
+        return string.Join(", ", macroDetails.Select(m =>
+            $"{m.Grams:F0} {m.Name} ({m.CalsPerGram * m.Grams / totalCals * 100:F1}%)"));
+    }
 }
 
 public static class MacrosExtensions
