688072f8ec2d2fcc98b321c896f8415d6518f8b3
Michael Buchoff <mbuchoff@gmail.com>
2024-05-27 14:29:05 -0400

    Parallelized things - too slow

--- Todoist.cs
+++ Todoist.cs
@@ -1,10 +1,9 @@
-﻿using System.Net.Http;
-using System.Net.Http.Json;
+﻿using System.Net.Http.Json;
 using SystemOfEquations.Data;
 
 namespace SystemOfEquations;
 
-internal class Todoist : IDisposable
+internal class Todoist
 {
     public async Task PushAsync(Phase phase)
     {
@@ -12,21 +11,24 @@ internal class Todoist : IDisposable
         var project = await GetOrCreateProjectAsync("Automation");
         Console.WriteLine(" Done");
 
-        Console.Write("Getting preexiting tasks from project...");
-        var todoistTasks = await GetTasksFromProjectAsync(project.Id);
-        Console.WriteLine(" Done");
-
-        foreach (var todoistTask in todoistTasks)
+        var deleteTask = new Task(async () =>
         {
-            if (todoistTask.Parent_Id == null)
-            {
-                Console.Write($"Deleting {todoistTask.Content}...");
-                await DeleteTaskAsync(todoistTask.Id);
-                Console.WriteLine(" Done");
-            }
-        }
+            Console.WriteLine("Getting preexiting tasks from project...");
+            var todoistTasks = await GetTasksFromProjectAsync(project.Id);
+            Console.WriteLine("Done getting preexiting tasks from project");
 
-        foreach (var x in new[]
+            await Task.WhenAll(todoistTasks
+                .Where(todoistTask => todoistTask.Parent_Id == null)
+                .Select(async todoistTask =>
+                {
+                    Console.WriteLine($"Deleting {todoistTask.Content}...");
+                    await DeleteTaskAsync(todoistTask.Id);
+                    Console.WriteLine($"Done deleting {todoistTask.Content}");
+                }).ToList());
+        });
+        deleteTask.Start();
+
+        var addTasks = new[]
         {
             phase.TrainingWeek.XFitDay,
             phase.TrainingWeek.RunningDay,
@@ -35,7 +37,7 @@ internal class Todoist : IDisposable
         {
             trainingDay.TrainingDayType,
             Meal = meal,
-        })))
+        })).Select(async x =>
         {
             if (x.Meal.FoodGrouping.PreparationMethod == FoodGrouping.PreparationMethodEnum.PrepareAsNeeded)
             {
@@ -47,28 +49,28 @@ internal class Todoist : IDisposable
                     { TrainingDayTypes.NonweightTrainingDay, "every sat,sun" },
                 }.GetValueOrDefault(x.TrainingDayType);
 
-                Console.Write($"Adding {content}...");
+                Console.WriteLine($"Adding {content}...");
                 var parentTodoistTask = await AddTaskAsync(content, dueString, parentId: null, project.Id);
-                Console.WriteLine(" Done");
+                Console.WriteLine($"Done adding {content}");
 
-                foreach (var helping in x.Meal.Helpings)
+                await Task.WhenAll(x.Meal.Helpings.Where(h => !h.Food.IsConversion).Select(async h =>
                 {
-                    if (!helping.Food.IsConversion)
-                    {
-                        Console.Write($"Adding {helping} to {content}...");
-                        await AddTaskAsync(
-                            helping.ToString(), dueString: null, parentTodoistTask.Id, projectId: null);
-                        Console.WriteLine(" Done");
-                    }
-                }
+                    Console.WriteLine($"Adding {h} to {content}...");
+                    await AddTaskAsync(
+                        h.ToString(), dueString: null, parentTodoistTask.Id, projectId: null);
+                    Console.WriteLine($"Done adding {h} to {content}");
+                }));
             }
-        }
+        }).ToList();
+
+        await deleteTask;
+        await Task.WhenAll(addTasks);
     }
 
     private async Task<TodoistTask> AddTaskAsync(
         string content, string? dueString, string? parentId, string? projectId)
     {
-        var httpClient = await _httpClientTask;
+        using var httpClient = await CreateHttpClientAsync();
         var result = await httpClient.PostAsJsonAsync("https://api.todoist.com/rest/v2/tasks", new
         {
             Content = content,
@@ -83,14 +85,14 @@ internal class Todoist : IDisposable
 
     private async Task DeleteTaskAsync(string id)
     {
-        var httpClient = await _httpClientTask;
+        using var httpClient = await CreateHttpClientAsync();
         var deleteResult = await httpClient.DeleteAsync($"https://api.todoist.com/rest/v2/tasks/{id}");
         deleteResult.EnsureSuccessStatusCode();
     }
 
     private async Task<TodoistTask[]> GetTasksFromProjectAsync(string id)
     {
-        var httpClient = await _httpClientTask;
+        using var httpClient = await CreateHttpClientAsync();
         var tasksResponse = await httpClient.GetAsync($"https://api.todoist.com/rest/v2/tasks?project_id={id}");
         var todoistTasks = await tasksResponse.Content.ReadFromJsonAsync<TodoistTask[]>();
         return todoistTasks ?? throw new NullReferenceException(nameof(TodoistTask));
@@ -103,7 +105,7 @@ internal class Todoist : IDisposable
 
         if (project == null)
         {
-            var httpClient = await _httpClientTask;
+            using var httpClient = await CreateHttpClientAsync();
             var projectsResponse = await httpClient.PostAsJsonAsync(
                 "https://api.todoist.com/rest/v2/projects", new { Name = projectName });
             project = await projectsResponse.Content.ReadFromJsonAsync<Project>();
@@ -114,7 +116,7 @@ internal class Todoist : IDisposable
 
     private async Task<Project[]> GetProjectsAsync()
     {
-        var httpClient = await _httpClientTask;
+        using var httpClient = await CreateHttpClientAsync();
         var projectsResponse = await httpClient.GetAsync("https://api.todoist.com/rest/v2/projects");
         projectsResponse.EnsureSuccessStatusCode();
         var projects = await projectsResponse.Content.ReadFromJsonAsync<Project[]>();
@@ -125,20 +127,20 @@ internal class Todoist : IDisposable
         return projects;
     }
 
-    public void Dispose()
+    private string? _apiKey;
+    private async Task<string> GetApiKeyAsync()
     {
-        _httpClientTask.ContinueWith(task => task.Result.Dispose());
+        _apiKey ??= await File.ReadAllTextAsync(@"C:\Users\mbuch\OneDrive\Desktop\secrets\todoist.txt");
+        return _apiKey;
     }
 
-    private readonly Task<HttpClient> _httpClientTask =
-        File.ReadAllTextAsync(@"C:\Users\mbuch\OneDrive\Desktop\secrets\todoist.txt").ContinueWith(readAllTextTask =>
-        {
-            var apiKey = readAllTextTask.Result;
-            var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
-            return httpClient;
-        });
-
+    private async Task<HttpClient> CreateHttpClientAsync()
+    {
+        var apiKey = await GetApiKeyAsync();
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+        return httpClient;
+    }
     private record Project(string Id, string Name);
     private record TodoistTask(string Content, string Id, string? Parent_Id);
 }
