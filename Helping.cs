f8de3d4cb389913cdb8c70db3dedc7b3a3a78833
Michael Buchoff <mbuchoff@gmail.com>
2024-12-01 19:30:31 -0500

    Fixed errors in Helping.cs

--- Helping.cs
+++ Helping.cs
@@ -1,19 +1,20 @@
 namespace SystemOfEquations;
 
-internal record Helping(Food Food, double Servings)
+internal record Helping(Food Food, ServingUnit ServingUnit, double Servings)
 {
-    public override string ToString() => Food.ToString(Servings);
+    public override string ToString() => Food.ToString(ServingUnit, Servings);
 
     public Macros Macros => Food.NutritionalInformation.Macros * Servings;
     public NutritionalInformation NutritionalInformation => Food.NutritionalInformation * Servings;
-    public static Helping operator *(Helping h, double d) => new(h.Food, h.Servings * d);
+    public static Helping operator *(Helping h, double d) => new(h.Food, h.ServingUnit, h.Servings * d);
 }
 
 internal static class HelpingExtensions
 {
     public static IEnumerable<Helping> CombineLikeHelpings(this IEnumerable<Helping> helpings) => helpings
-        .GroupBy(h => h.Food)
+        .GroupBy(h => (h.Food, h.ServingUnit))
         .Select(foodGrouping => new Helping(
-            Food: foodGrouping.Key,
+            foodGrouping.Key.Food,
+            foodGrouping.Key.ServingUnit,
             Servings: foodGrouping.Select(h => h.Servings).Sum()));
 }
