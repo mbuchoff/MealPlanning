f895e919b408c72d19b28e5cbbeabbc3b4631b39
Michael Buchoff <mbuchoff@gmail.com>
2025-05-13 07:30:23 -0400

    3100 cal/day

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -2,8 +2,8 @@
 using SystemOfEquations.Data.TrainingWeeks;
 using SystemOfEquations.Todoist;
 
-// 3200
-var percentIncrease = 30.75M;
+// 3100
+var percentIncrease = 24.75M;
 var trainingWeek = new Base().PlusPercent(100 + percentIncrease);
 var phase = new Phase($"{trainingWeek.Name}, plus {percentIncrease}%", trainingWeek);
 
